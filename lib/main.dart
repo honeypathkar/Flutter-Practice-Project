2cca96a6017d059c34f7bf995e0d6e5f82d7a6c1
honeypathkar <honeypatkar70@gmail.com>
2024-08-24 13:47:41 +0530

    Regenerating main.dart file form start

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyFlutterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+// ignore: use_key_in_widget_constructors
+class MyFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Home'),
+      title: "Flutter Demo App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var arrName = [
-    'Honey',
-    'Shivansh',
-    'Harsh',
-    'Gaurav',
-    'Shivam',
-    'Shubham',
-    'Nikhil'
-  ];
-
+// ignore: use_key_in_widget_constructors
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        // ignore: sized_box_for_whitespace
-        body: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Text(arrName[index],
-                    style: const TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.bold)));
-          },
-          itemCount: arrName.length,
-        ));
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Container(
+        color: Colors.cyan,
+      ),
+    );
   }
 }
