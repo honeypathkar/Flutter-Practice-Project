b2eec47402a8b859c1331e5c6060b1744181df3a
honeypathkar <honeypatkar70@gmail.com>
2024-08-24 14:35:54 +0530

    Adding padding and margin to text

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,26 @@ class MyHomePage extends StatelessWidget {
           child: Container(
             width: 200,
             height: 200,
+            // ignore: sort_child_properties_last
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'Hello world!',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                // ignore: avoid_unnecessary_containers
+                Container(
+                  margin: const EdgeInsets.all(5),
+                  child: const Text(
+                    'Hello world!',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                )
+              ],
+            ),
             decoration: BoxDecoration(
                 color: Colors.indigo.shade500,
                 // ignore: prefer_const_constructors
