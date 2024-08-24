7ee03fc96451fa7f8a1462b031a9451a0f1981f2
honeypathkar <honeypatkar70@gmail.com>
2024-08-24 14:06:49 +0530

    boxDecoration function in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyFlutterApp extends StatelessWidget {
     return MaterialApp(
       title: "Flutter Demo App",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.grey),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
       home: MyHomePage(),
     );
   }
@@ -24,9 +27,26 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: Container(
-        color: Colors.cyan,
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.indigo.shade300,
+        child: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+                color: Colors.indigo.shade500,
+                // ignore: prefer_const_constructors
+                borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(20),
+                    bottomRight: const Radius.circular(20)),
+                border: Border.all(color: Colors.black, width: 2.0),
+                boxShadow: const [BoxShadow(blurRadius: 5, spreadRadius: 2)]),
+          ),
+        ),
       ),
     );
   }
