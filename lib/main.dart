04b501e43664e54842a2921ce6c350d0361860ce
honeypathkar <honeypatkar70@gmail.com>
2024-08-24 14:43:30 +0530

    Using ListTile for designing list

--- lib/main.dart
+++ lib/main.dart
@@ -20,54 +20,34 @@ class MyFlutterApp extends StatelessWidget {
   }
 }
 
-// ignore: use_key_in_widget_constructors
+// ignore: use_key_in_widget_constructors, must_be_immutable
 class MyHomePage extends StatelessWidget {
+  var arrName = [
+    'Honey',
+    'Shivansh',
+    'Harsh',
+    'Gaurav',
+    'Shivam',
+    'Shubham',
+    'Nikhil'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.indigo.shade300,
-        child: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            // ignore: sort_child_properties_last
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    'Hello world!',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ),
-                // ignore: avoid_unnecessary_containers
-                Container(
-                  margin: const EdgeInsets.all(5),
-                  child: const Text(
-                    'Hello world!',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                )
-              ],
-            ),
-            decoration: BoxDecoration(
-                color: Colors.indigo.shade500,
-                // ignore: prefer_const_constructors
-                borderRadius: BorderRadius.only(
-                    topLeft: const Radius.circular(20),
-                    bottomRight: const Radius.circular(20)),
-                border: Border.all(color: Colors.black, width: 2.0),
-                boxShadow: const [BoxShadow(blurRadius: 5, spreadRadius: 2)]),
-          ),
+        appBar: AppBar(
+          title: const Text("Home"),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
-      ),
-    );
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Text('${index + 1}'),
+              title: Text(arrName[index]),
+              subtitle: const Text('Number'),
+              trailing: const Icon(Icons.signal_wifi_4_bar),
+            );
+          },
+          itemCount: arrName.length,
+        ));
   }
 }
