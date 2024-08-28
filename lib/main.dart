ee4ababc0b5f87938f88750472e258365eda4e74
honeypathkar <honeypatkar70@gmail.com>
2024-08-28 14:25:25 +0530

    Wrap widget in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -35,40 +35,93 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // var time = DateTime.now();
-  var colors = [
-    Colors.pink,
-    Colors.red,
-    Colors.deepPurple,
-    Colors.indigoAccent,
-    Colors.yellow,
-    Colors.green,
-    Colors.blue,
-    Colors.purple,
-    Colors.indigo
-  ];
+  // var colors = [
+  //   Colors.pink,
+  //   Colors.red,
+  //   Colors.deepPurple,
+  //   Colors.indigoAccent,
+  //   Colors.yellow,
+  //   Colors.green,
+  //   Colors.blue,
+  //   Colors.purple,
+  //   Colors.indigo
+  // ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // ignore: sized_box_for_whitespace, avoid_unnecessary_containers
-        child: Container(
-            child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
-          itemBuilder: (context, index) {
-            return Container(
-              color: colors[index],
-            );
-          },
-          itemCount: colors.length,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        // ignore: avoid_unnecessary_containers, sized_box_for_whitespace
+        body: Container(
+          width: double.infinity,
+          child: Wrap(
+            alignment: WrapAlignment.spaceEvenly,
+            spacing: 11,
+            runSpacing: 11,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.indigo,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.purple,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.grey,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.lightBlue,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.indigoAccent,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.deepPurple,
+              )
+            ],
+          ),
         )
+        //     child: GridView.builder(
+        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //       crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+        //   itemBuilder: (context, index) {
+        //     return Container(
+        //       color: colors[index],
+        //     );
+        //   },
+        //   itemCount: colors.length,
+        // )
 
-            /* GridView.count(
+        /* GridView.count(
           crossAxisCount: 3,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
@@ -102,8 +155,7 @@ class _MyHomePageState extends State<MyHomePage> {
             )
           ],
         ) */
-            ),
-      ),
-    );
+
+        );
   }
 }
